feb12191548a210824c6c1f774f073584ce7b7ae
rjdroege <99696219+rjdroege@users.noreply.github.com>
2022-03-21 20:55:56 -0500

    Exercise Two
    Class Eleven Exercise Two

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { post } from './post/post.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    post
   ],
   imports: [
     BrowserModule
